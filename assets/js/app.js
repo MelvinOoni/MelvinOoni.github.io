8e301507ace8bd52d03c7ce6c94549c990e2e70a
melvinooni <melvinoonincx2@gmail.com>
2018-10-07 19:50:03 +0200

    Revert "verandering"
    This reverts commit 14461072c8f4fce2bf7802d2768e98d0d0fadf75.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,94 +1,143 @@
-let me = {
-    name : 'Melvin Oonincx',
-    age : 20,
-    previousDiploma : 'MBO Bedrijfsadministrateur',
-    birthPlace: 'Wouw'
+
+const firstname = "Melvin";
+const lastname = "Oonincx";
+const age = "20";
+
+console.log('Hallo, ik ben ' + firstname + ' ' + lastname + ' en ik ben ' + age + ' jaar oud');
+console.log(`Mijn voornaam is ${firstname}`);
+
+const howmanydays = 32 + 40;
+
+console.log(howmanydays);
+
+const priceOfAnApple = 0.59,
+    numberOfApples = 200;
+const totalPrice = priceOfAnApple * numberOfApples;
+
+console.log(totalPrice)
+console.log(`Totaalprijs is : ${totalPrice}`);
+
+const str = 'Please visit the HZ University of applied science'
+console.log(str.replace('applied', 'mad'));
+
+const checkIfNumberIsEven = 20;
+
+if (checkIfNumberIsEven % 2 == 0) {
+    console.log('Het nummer is even');
 }
-console.log(me);
-
-me.currentStudy = 'HBO-ICT';
-me.hobby = 'voetbal';
-
-console.log(me);
-
-me.favouriteVehicle = {
-    vehicle : 'car',
-    brand : 'jaguar',
-    model : 'F-pace',
-    wheels : 4
-}
-
-// console.log(`Mijn favoriete vervoersmiddel is ${me.favouriteVehicle.vehicle} en deze heeft ${me.favouriteVehicle.wheels} wielen.`)
-
-me.family = [
-        'Jan', 
-        'Kees', 
-        'Piet',
-        'Peter'
-]
-
-for (let i = 0; i < me.family.length; i++) {
-    console.log(me.family[i]);
+else {
+    console.log('Het nummer is oneven');
 }
 
+const HZtekst = 'Programming is not so cool';
+console.log(HZtekst.replace('not ', ''));
 
+//Volgens de code heb ik in de HZtekst het woord not vervangen met een spatie.
 
-let logFavouriteVehicle = function(){
-    console.log(`Mijn favoriete vervoersmiddel is ${me.favouriteVehicle.vehicle} en deze heeft ${me.favouriteVehicle.wheels} wielen.`)
-}
-let logDropDown = function(){
-    console.log(me.family[i]);
-}
+const Value = '1400';
+const Naboo = 'Ik woon in Naboo';
+const Comparison = Value == Naboo;
+console.log(Comparison);
 
-console.log(logFavouriteVehicle());
+/*De vergelijking tussen Value en Naboo is false, dit komt omdat het niet hetzelfde is.
+Het is niet handig om waardes met letters te vergelijken. */
 
-const lapRounds = {
-    roundTimes: [55.99,  63.00, 63.01, 54.01, 62.79, 52.88, 53.10, 54.12]
-};
-console.log(lapRounds)
+const currentAction = 'strings';
+if (currentAction == 'calculating') {
+    console.log('in Calculating');
+    const a = 10
+    const b = 11
+    console.log('optellen', a + b);
 
-lapRounds.roundTimes = ["55.99",  "63.00", "63.01", "54.01", "62.79", "52.88", "53.10", "54.12"]; 
-console.log(lapRounds)
-
-//Arrays zijn objects.
-
-const teachers = [
-    {
-        name: "Loek",
-        profession: "Teacher",
-        brand: "Linux"
-    },
-    {
-        name: "Daan",
-        profession: "Teacher",
-        brand: "Arduino"
-    },
-    {
-        name: "Rimmert",
-        profession: "Teacher",
-        brand: "Apple"
+} else if (currentAction == 'strings') {
+    console.log('in strings');
+    const myString = 'My beatiful string';
+    console.log(myString.toUpperCase());
+    console.log('character at', myString.charAt(7));
+    console.log('lengte van de string', myString.length)
+    console.log('substring', myString.substring(3, 8))
+} else if (currentAction == 'booleans') {
+    console.log('in booleans');
+    const punt = 7
+    if (punt > 8) {
+        console.log('true')
+    } else {
+        console.log('false')
     }
-]
+} else {
+    console.error('help!')
+};
 
-for (let index = 0; index < teachers.length; index++) {
-    const element = teachers[index];
-    console.log(teachers[index]);
-    if (teachers.profession === "Teacher") {
-        if (teachers.name === "Loek") {
-            if (teachers.brand === "linux") {
-                console.log(`I have a ${teachers.profession} named ${teachers.name} and he likes to work on a ${teachers.brand} computer.`)
-            }
-        } else if (teachers.name === "Daan") {
-            if (teachers.brand === "Arduino") {
-                console.log(`I have a ${teachers.profession} named ${teachers.name} and he likes to work on a ${teachers.brand} computer.`)
-            }
-        } else if (teachers.name === "Rimmert") {
-            if (teachders.brand === "Apple") {
-                console.log(`I have a ${teachers.profession} named ${teachers.name} and he likes to work on a ${teachers.brand} computer.`)
+const gewicht = 75
+const lengte = 1.88
+const BMI = Math.round(gewicht / (lengte * lengte)*10)/10;
+console.log(BMI)
+
+if (BMI < 18.5) {
+    console.log ('ondergewicht');
+} else {
+    if (BMI >= 18.5 && 24.9 >= BMI) {
+    console.log ('normaal gewicht');
+    } else {
+        if (BMI >= 25 && 29.9 >= BMI) {
+            console.log('overgewicht');
+        } else {
+            console.log('obesitas');
+        }
+    }
+}
+
+const grade = 10
+console.log (grade)
+
+if (grade <6) {
+    console.log ("onvoldoende");
+} else {
+    } if (grade >=6 && 7 >= grade) {
+        console.log ("voldoende");
+        } else {
+            if (grade > 7 && 9 >= grade) {
+            console.log ("goed");
+            } else {
+                if (grade > 9) {
+                console.log ("uitmuntend");
             }
         }
-    } else {
-        console.log(`Probeer opnieuw`)
     }
-};
 
+    switch (grade) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5: 
+          console.log('onvoldoende');
+          break;
+        case 6:
+        case 7:
+          console.log('voldoende');
+          break;
+        case 8:
+        case 9:
+          console.log('goed');
+          break;
+        case 10:
+          console.log('uitmuntend');
+          break;
+        default:
+          console.log('Sorry');
+      }
+const purchasedBook = true;
+const job = 'teacher';
+const inTrain = false;
+
+if (purchasedBook && inTrain == true) {
+    if (job == 'teacher') {
+        console.log ('finally i can enjoy my book!')
+    }
+    else {
+        console.log ('I cant enjoy my book!')
+    }
+} else {
+    console.log('I cant enjoy my book!')
+}
